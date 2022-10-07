648b51f4b820b4a09b3587543c3b90a9edac1528
AleVVis <alejandra.visconti@hotmail.com>
2022-10-07 00:42:00 -0300

    Primer Commit con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './Modals/login/login.component';
+import { RedesComponent } from './redes/redes.component';
+import { SobremiComponent } from './sobremi/sobremi.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { IdiomasComponent } from './idiomas/idiomas.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
+import { IntroComponent } from './intro/intro.component';
+import { NavbarDashboardComponent } from './navbar-dashboard/navbar-dashboard.component';
+import { ButtonLoginComponent } from './button-login/button-login.component';
+import { ButtonLogoutComponent } from './button-logout/button-logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    RedesComponent,
+    SobremiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    IdiomasComponent,
+    ProyectosComponent,
+    DashboardComponent,
+    ErrorComponent,
+    IntroComponent,
+    NavbarDashboardComponent,
+    ButtonLoginComponent,
+    ButtonLogoutComponent
   ],
   imports: [
     BrowserModule,
